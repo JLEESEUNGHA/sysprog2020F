5553242019e87371dad49f5d2a08dfdd0e0429a9
JLEESEUNGHA <jleesh@korea.ac.kr>
2020-11-11 23:41:59 +0900

    tested connect().

--- assignment2/client.c
+++ assignment2/client.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
-#ifdef _WIN32
+#ifdef _WIN32 // for development on Windows (thru MinGW)
     #include <winsock.h>
 #else
     #include <sys/socket.h>
+    #include <sys/types.h>
+    #include <netinet/in.h>
 #endif
 
 #define BUF_LEN 256
@@ -12,10 +15,10 @@
 int test_thread(void *test_data);
 
 int main(int argc, char *argv[]) {
-    char buffer[BUF_LEN];
+    char buf[BUF_LEN];
     struct sockaddr_in server_addr, client_addr;
     int client_fd;
-    int n_port, target_port[10];
+    int n_port, target_port[BUF_LEN];
     
 
     // read the number of ports to create
@@ -26,6 +29,7 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &n_port);
     //printf("%d\n", n_port);
 
+    // save port addresses
     for (int i = 0; i < n_port; ++i) {
         //printf("%s\n", argv[i + 2]);
         if (argv[i + 2] == NULL) {
@@ -36,17 +40,19 @@ int main(int argc, char *argv[]) {
     }
 
 
+    // create socket(s)
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         printf("Client: Can't open stream socket!\n");
-        scanf("%s", &buffer);
         return -1;
     }
 
+    // initialise server_addr 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(target_port);
-    server_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    server_addr.sin_port = htons(1111);
+    server_addr.sin_addr.s_addr = inet_addr("192.168.56.101");
 
+    // attempt connection(s)
     ///*
     if (connect(client_fd,
                 (struct sockaddr*) &server_addr,
@@ -54,8 +60,19 @@ int main(int argc, char *argv[]) {
         printf("Connection failed.\n");
         return -1;
     }
+    while (1) {
+        if (read(client_fd, buf, 0) == 0) {
+            continue;
+        }
+        printf("%s", buf);
+        if (strcmp(buf, "@@@@@")) {
+            break;
+        }
+    }
     //*/
 
+
+    // test threads
     ///*
     pthread_t p_thread;
     int th_id;
